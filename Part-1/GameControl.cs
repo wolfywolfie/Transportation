6ff9d22391ba91fef571d56219bda47163c2177a
wolfywolfie <31907254+wolfywolfie@users.noreply.github.com>
2017-10-25 18:57:14 -0400

    Add files via upload

---
+++ Part-1/GameControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+    public GameObject mainCube;
+    public GameObject currentCube;
+    public static GameObject oldCube;
+    Vector3 cubePosition;
+
+    // Use this for initialization
+    void Start()
+    {
+        //spawns cubes
+            for (int x = -11; x < 20; x=x+2)
+            {
+                cubePosition = new Vector3(x, 1, 0);
+                Instantiate(mainCube, cubePosition, Quaternion.identity);
+            }
+
+    }
+
+
+
+    public static void ProcessClick(GameObject currentCube)
+    {
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Part-1/cubeControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeControl : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+
+        if (GameControl.oldCube != null)
+        {
+            GameControl.oldCube.GetComponent<Renderer>().material.color = Color.white;
+        }
+        
+        GameControl.oldCube = this.gameObject;
+
+    }
+}
