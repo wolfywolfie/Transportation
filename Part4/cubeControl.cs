feb84c9a25a740ff45ad33edd43a3c53059e0c20
wolfywolfie <31907254+wolfywolfie@users.noreply.github.com>
2017-11-08 20:00:24 -0500

    Add files via upload

---
+++ Part4/cubeControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeControl : MonoBehaviour
+{
+    GameControl myGameController;
+
+    public int myX, myY;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        GameControl.ProcessClick(gameObject, myX, myY);
+
+    }
+}
